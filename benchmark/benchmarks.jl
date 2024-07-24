978e6b59e3d341e90f8d737623cb72658ce5937e
Lorenzo Varese <55581163+lorenzovarese@users.noreply.github.com>
2024-07-24 13:50:01 +0200

    Add commented benchmark for field operations

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -19,4 +19,20 @@ af = Field((Cell, K), rand(1000, 1000)); bf = Field((Cell, K), rand(1000, 1000))
 SUITE["arith_broadcast"]["arrays"] = @benchmarkable a .+ b .- c
 SUITE["arith_broadcast"]["fields"] = @benchmarkable af .+ bf .- cf
 
+# Benchmark for field operator addition
+
+# function benchmark_fo_addition()
+#     a = Field(Cell, collect(1.0:15.0))
+#     b = Field(Cell, collect(-1.0:-1:-15.0))
+#     out = Field(Cell, zeros(Float64, 15))
+
+#     @field_operator function fo_addition(a::Field{Tuple{Cell_},Float64}, b::Field{Tuple{Cell_},Float64})::Field{Tuple{Cell_},Float64}
+#         return a .+ b
+#     end
+
+#     @benchmarkable fo_addition(a, b, backend="embedded", out=out)
+# end
+
+# SUITE["field_operator"]["addition"] = benchmark_fo_addition()
+
 run(SUITE, verbose = true, seconds = 1)
