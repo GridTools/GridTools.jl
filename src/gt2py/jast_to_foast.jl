1bbb1e64d8d88aa374d91dc1418c35bc9a8449fb
Lorenzo Varese <55581163+lorenzovarese@users.noreply.github.com>
2024-08-16 11:56:53 +0200

    Quick fix to the unary/binary operation support

--- src/gt2py/jast_to_foast.jl
+++ src/gt2py/jast_to_foast.jl
@@ -266,6 +266,7 @@ end
 
 function visit_(sym::Val{:call}, args::Array, outer_loc)
     if args[1] in bin_op
+        # TODO: check the case where a unary expression, that is at the same time binary operation is encountered: i.e. -x
         @assert length(args)==3 "Expected a binary operation. AST must be canonicalized using `canonicalize_arithmetic_ops` first."
         return foast.BinOp(
             op = visit(args[1]),
