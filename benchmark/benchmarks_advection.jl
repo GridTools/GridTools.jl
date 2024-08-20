391dc0a269a5254d7e8fc47e61665bf7b44ae082
Lorenzo Varese <55581163+lorenzovarese@users.noreply.github.com>
2024-08-20 10:53:21 +0200

    Fix K dimension in advection meshes

--- benchmark/benchmarks_advection.jl
+++ benchmark/benchmarks_advection.jl
@@ -26,22 +26,22 @@ advection_suite["advection"]["upwind_julia_embedded"] = @benchmarkable upwind_sc
         # embedded backend
     )
 
-# advection_suite["advection"]["upwind_python_backend"] = @benchmarkable upwind_scheme(
-#         state.rho,
-#         δt,
-#         mesh.vol,
-#         metric.gac,
-#         state.vel[1],
-#         state.vel[2],
-#         state.vel[3],
-#         mesh.pole_edge_mask,
-#         mesh.dual_face_orientation,
-#         mesh.dual_face_normal_weighted_x,
-#         mesh.dual_face_normal_weighted_y,
-#         out = state_next.rho,
-#         offset_provider = mesh.offset_provider,
-#         backend = "py"
-#     )
+advection_suite["advection"]["upwind_python_backend"] = @benchmarkable upwind_scheme(
+        state.rho,
+        δt,
+        mesh.vol,
+        metric.gac,
+        state.vel[1],
+        state.vel[2],
+        state.vel[3],
+        mesh.pole_edge_mask,
+        mesh.dual_face_orientation,
+        mesh.dual_face_normal_weighted_x,
+        mesh.dual_face_normal_weighted_y,
+        out = state_next.rho,
+        offset_provider = mesh.offset_provider,
+        backend = "py"
+    )
 
 advection_suite["advection"]["mpdata_program_julia_embedded"] = @benchmarkable mpdata_program(
         state.rho,
@@ -84,9 +84,9 @@ println("Running the advection suite...")
 advection_results = run(advection_suite)
 
 upwind_embedded_results = advection_results["advection"]["upwind_julia_embedded"]
-# upwind_python_backend_results = results["advection"]["upwind_python_backend"]
+upwind_python_backend_results = advection_results["advection"]["upwind_python_backend"]
 mpdata_embedded_results = advection_results["advection"]["mpdata_program_julia_embedded"]
-# mpdata_python_backend_results = results["advection"]["mpdata_program_python_backend"]
+# mpdata_python_backend_results = advection_results["advection"]["mpdata_program_python_backend"]
 
 # Function to convert nanoseconds to milliseconds for clearer output
 ns_to_ms(time_ns) = time_ns / 1e6
@@ -94,8 +94,8 @@ ns_to_ms(time_ns) = time_ns / 1e6
 println("Upwind scheme julia (embedded):")
 println("\tTime taken: $(ns_to_ms(median(upwind_embedded_results.times))) ms\n")
 
-# println("Upwind scheme julia (python backend):")
-# println("\tTime taken: $(ns_to_ms(median(upwind_python_backend_results.times))) ms\n")
+println("Upwind scheme julia (python backend):")
+println("\tTime taken: $(ns_to_ms(median(upwind_python_backend_results.times))) ms\n")
 
 println("Mpdata program julia (embedded):")
 println("\tTime taken: $(ns_to_ms(median(mpdata_embedded_results.times))) ms\n")
